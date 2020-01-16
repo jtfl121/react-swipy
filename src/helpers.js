0a89a8e00704966eb4862a9f4e04e27686b17d1d
Joshua <torres_306@yahoo.com>
2020-01-16 15:16:31 -0500

    update to direction

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,7 @@ export const getOffset = (start, end) => -((start - end) * 0.75);
 export const getEvent = e => (e.touches ? e.touches[0] : e);
 export const withY = fn => e => fn(getEvent(e).pageY);
 export const getLimitOffset = (limit, direction) =>
-  direction === "up" ? limit : -limit;
+  direction === "down" ? limit : -limit;
 export const getOpacity = (offset, limit, min) =>
   1 -
   (Math.abs(offset) < min
